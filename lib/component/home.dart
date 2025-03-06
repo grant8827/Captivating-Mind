7bd24cc0c589b2dfa11876c06034f1a6492c6e2f
Gregory Grant <grant8827@yahoo.com>
2025-03-05 20:07:36 -0500

    first commit

---
+++ lib/component/home.dart
@@ -0,0 +1,228 @@
+import 'package:captivating_mind/component/radio.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({super.key});
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 136, 9, 0),
+        iconTheme: const IconThemeData(color: Colors.white, size: 35),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await Share.share(
+                  'https://apps.apple.com/us/app/captivating-mind/id6504638762');
+            },
+            icon: const Icon(Icons.share),
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('lib/assets/image/bgimage.jpg'),
+              fit: BoxFit.fill),
+        ),
+        child: radioPlayer(),
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color.fromARGB(255, 221, 221, 221),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+                decoration:
+                    BoxDecoration(color: Color.fromARGB(255, 136, 9, 0)),
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage(
+                        'lib/assets/image/captivating_ming.png',
+                      ),
+                      width: 100,
+                      height: 80,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Captivating Mind Radio',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    )
+                  ],
+                )),
+            ListTile(
+              leading: const Icon(Icons.phone,
+                  size: 35, color: Color.fromARGB(255, 23, 21, 12)),
+              title: const Text('407-234-9036',
+                  style: TextStyle(fontSize: 18, color: Colors.black)),
+              onTap: () async {
+                Uri phone = Uri.parse('tel:407-234-9036');
+                if (await launchUrl(phone)) {
+                  //dialer open
+                } else {
+                  //do nothing
+                }
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.phone,
+                  size: 35, color: Color.fromARGB(255, 23, 21, 12)),
+              title: const Text('321-350-2773',
+                  style: TextStyle(fontSize: 18, color: Colors.black)),
+              onTap: () async {
+                Uri phone = Uri.parse('tel:321-350-2773');
+                if (await launchUrl(phone)) {
+                  //dialer open
+                } else {
+                  //do nothing
+                }
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.email,
+                size: 35,
+                color: Color.fromARGB(255, 23, 21, 12),
+              ),
+              title: const Text(
+                'captivating1mind@gmail.com',
+                style: TextStyle(fontSize: 18),
+              ),
+              onTap: () async {
+                Uri email = Uri.parse('mailto:captivating1mind@gmail.com');
+                if (await launchUrl(email)) {
+                  //open email
+                } else {
+                  //do nothing
+                }
+                Navigator.pop(context);
+              },
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.facebook,
+                  color: Color.fromARGB(255, 0, 140, 255),
+                  size: 35,
+                ),
+                title: const Text('Facebook'),
+                onTap: () async {
+                  Uri email = Uri.parse('https://search.app/sc94q9M66iArER156');
+                  if (await launchUrl(email)) {
+                    //open email
+                  } else {
+                    //do nothing
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(
+                  FontAwesomeIcons.instagram,
+                  color: Colors.pink,
+                  size: 35,
+                ),
+                title: const Text('instagram'),
+                onTap: () async {
+                  Uri email = Uri.parse(
+                      'https://www.instagram.com/captivating1mind/?hl=en');
+                  if (await launchUrl(email)) {
+                    //open email
+                  } else {
+                    //do nothing
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(
+                  FontAwesomeIcons.tiktok,
+                  color: Colors.black,
+                  size: 35,
+                ),
+                title: const Text('TikTak'),
+                onTap: () async {
+                  Uri email = Uri.parse('#');
+                  if (await launchUrl(email)) {
+                    //open email
+                  } else {
+                    //do nothing
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(
+                  FontAwesomeIcons.twitter,
+                  color: Color.fromARGB(255, 66, 162, 240),
+                  size: 35,
+                ),
+                title: const Text('Twitter'),
+                onTap: () async {
+                  Uri email = Uri.parse('#');
+                  if (await launchUrl(email)) {
+                    //open email
+                  } else {
+                    //do nothing
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(
+                  FontAwesomeIcons.youtube,
+                  color: Colors.red,
+                  size: 35,
+                ),
+                title: const Text('YouTube'),
+                onTap: () async {
+                  Uri email = Uri.parse(
+                      'https://youtube.com/@captivatingmind3914?si=TL3io2z3KY-9Js0T');
+                  if (await launchUrl(email)) {
+                    //open email
+                  } else {
+                    //do nothing
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/component/radio.dart
@@ -0,0 +1,132 @@
+/*
+ *  main.dart
+ *
+ *  Created by Ilia Chirkunov <xc@yar.net> on 28.12.2020.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:radio_player/radio_player.dart';
+
+class radioPlayer extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<radioPlayer> {
+  RadioPlayer _radioPlayer = RadioPlayer();
+  bool isPlaying = false;
+  List<String>? metadata;
+
+  @override
+  void initState() {
+    super.initState();
+    initRadioPlayer();
+  }
+
+  void initRadioPlayer() {
+    _radioPlayer.setChannel(
+      title: 'Radio Player',
+      url: 'https://stream.zeno.fm/lo2dd6zbzifvv',
+      imagePath: 'lib/assets/image/captivating_ming.png',
+    );
+
+    _radioPlayer.stateStream.listen((value) {
+      setState(() {
+        isPlaying = value;
+      });
+    });
+
+    _radioPlayer.metadataStream.listen((value) {
+      setState(() {
+        metadata = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.black,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 30,
+            ),
+            FutureBuilder(
+              future: _radioPlayer.getArtworkImage(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                Image artwork;
+                if (snapshot.hasData) {
+                  artwork = snapshot.data;
+                } else {
+                  artwork = Image.asset(
+                    'lib/assets/image/captivating_ming.png',
+                    fit: BoxFit.cover,
+                  );
+                }
+                return Container(
+                  height: 280,
+                  width: 300,
+                  child: ClipRRect(
+                    child: artwork,
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 20.0,
+              ),
+              child: Text(
+                'Taking every thought Captive',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            Text(
+              metadata?[0] ?? 'Now Playing',
+              softWrap: false,
+              overflow: TextOverflow.fade,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Color.fromARGB(255, 136, 9, 0)),
+            ),
+            Text(
+              metadata?[1] ?? '',
+              softWrap: false,
+              overflow: TextOverflow.fade,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            const SizedBox(height: 50),
+            IconButton(
+              style: IconButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 136, 9, 0),
+                iconSize: 60,
+              ),
+              onPressed: () {
+                isPlaying ? _radioPlayer.pause() : _radioPlayer.play();
+              },
+              icon: Icon(
+                isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
